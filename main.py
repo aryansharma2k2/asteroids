42a864c75146fd209623f449b709452e065dc5af
aryansharma2k2 <inguvaaryan22@gmail.com>
2025-01-02 16:27:51 -0500

    Adding player.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
 import pygame
+from player import Player
 
 def main():
     pygame.init()
@@ -10,8 +11,10 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
     while True:
         screen.fill(color="black")
+        player.draw(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
