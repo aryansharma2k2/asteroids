0666eb7a38873cc0c5f0199812839bc9102fe9e5
aryansharma2k2 <inguvaaryan22@gmail.com>
2025-01-02 16:17:19 -0500

    Adding Sprite.

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print("Starting asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         screen.fill(color="black")
         for event in pygame.event.get():
@@ -15,5 +17,6 @@ def main():
                 return
 
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 main()
